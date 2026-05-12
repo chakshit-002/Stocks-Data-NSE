8aac96ef99a2160f0511095768cc4587f93b38d3
chakshit-002 <gunidiachakshit@gmail.com>
2026-05-12 13:21:32 +0530

    resolving some error p-2

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ app.use(cors({
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.options('*', cors());
+app.options('(.*)', cors());
 
 app.use(cookieParser());
 
